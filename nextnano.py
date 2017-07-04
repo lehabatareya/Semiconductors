9c12c8ea62bf00de7c56d137ccf68203d37b4d83
lehabatareya <liohich@yandex.ru>
2017-07-04 16:37:19 +0300

    Added Nextnano.py file

---
+++ nextnano.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Interaction between Python and Nextnano3 simulation program
+"""
+
+
+def load_wave_func(folder):
+    """Function is responsible for reading and loading into the program
+    of simulation results from Nextnano program output files.
+    Loads Wavefunctions"""
+    filename = folder + '\sg_1band1\cb001_qc001_sg001_deg001_neu_cmplx.dat'
+
+    print(filename)
